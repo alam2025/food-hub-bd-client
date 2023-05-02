1d0060099077da2a2ed13c413e48f17ff8e0f26d
alam2025 <alam13100852@gmail.com>
2023-05-03 00:43:04 +0600

    chef card complete

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
       return (
-            <div className='mt-auto'>
-                  <p>&copy; Allright resevered</p>
-            </div>
+            <footer className="bg-dark text-light pt-5 pb-3 mt-5">
+                  <Container>
+                        <Row>
+                              <Col sm={4} className="text-center text-sm-start">
+                                    <h5>Food-Hub-Bd</h5>
+                                    <p>A place for foodies</p>
+                              </Col>
+                              <Col sm={4} className="text-center">
+                                    <h5>Contact Us</h5>
+                                    <p>Phone: +8801XXXXXXXXX</p>
+                                    <p>Email: foodhubbd@gmail.com</p>
+                              </Col>
+                              <Col sm={4} className="text-center text-sm-end">
+                                    <h5>Follow Us</h5>
+                                    <p>Facebook</p>
+                                    <p>Twitter</p>
+                                    <p>Instagram</p>
+                              </Col>
+                        </Row>
+                        <Row className="mt-3">
+                              <Col sm={12} className="text-center">
+                                    <p>© 2023 Food-Hub-Bd. All rights reserved.</p>
+                              </Col>
+                        </Row>
+                  </Container>
+            </footer>
       );
 };
 
