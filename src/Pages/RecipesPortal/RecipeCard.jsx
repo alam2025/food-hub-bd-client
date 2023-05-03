7eb553fe5bb9f91e7df5eac73aa1079cc9c0aa0a
alam2025 <alam13100852@gmail.com>
2023-05-03 11:22:08 +0600

    toast add and disable btn

--- src/Pages/RecipesPortal/RecipeCard.jsx
+++ src/Pages/RecipesPortal/RecipeCard.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Card, Toast } from 'react-bootstrap';
 import Rating from 'react-rating';
+import { FaRegStar, FaStar } from "react-icons/fa";
+import { Toaster, toast } from 'react-hot-toast';
+
 
 const RecipeCard = ({ recipe }) => {
+      const [showToast, setShowToast] = useState(false);
+      const [isDisabled, setIsDisabled] = useState(false);
+
       console.log(recipe);
-      const { recipe_name, ingredients, cooking_method, rating } = recipe;
+      const { recipe_name, ingredients, cooking_method, rating, id } = recipe;
+
+      const handleClick = () => {
+           toast.success('This recipe is now one of your favorites!')
+            setIsDisabled(true);
+      }
       return (
             <Card className='mt-4'>
                   <Card.Header as="h5">{recipe_name}</Card.Header>
@@ -13,7 +24,7 @@ const RecipeCard = ({ recipe }) => {
                               <h4>Ingredients</h4>
                               <ul className=' ms-5'>
 
-                                    {ingredients?.map((i,index) => <li key={index}>{i}</li>)}
+                                    {ingredients?.map((i, index) => <li key={index}>{i}</li>)}
                               </ul>
                         </div>
                         <div>
@@ -21,16 +32,29 @@ const RecipeCard = ({ recipe }) => {
                               <p>{cooking_method}</p>
                         </div>
 
-                        <Rating
-                              placeholderRating={3.5}
-                              emptySymbol={<img src="assets/images/star-grey.png" className="icon" />}
-                              placeholderSymbol={<img src="assets/images/star-red.png" className="icon" />}
-                              fullSymbol={<img src="assets/images/star-yellow.png" className="icon" />}
-                        />
+                        <div>
+                              <Rating
+                                    placeholderRating={rating}
+                                    readonly
+                                    emptySymbol={<FaRegStar />}
+                                    placeholderSymbol={<FaStar className=' text-warning ' />}
+                                    fullSymbol={<FaStar />}
+                              />
+                        </div>
 
 
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button
+                              className='btn-primary border-0'
+                              onClick={handleClick}
+                              disabled={isDisabled}
+                        >
+                              Favorite button
+                        </Button>
+
+                        
                   </Card.Body>
+                  <Toaster />
+
             </Card>
       );
 };
